364b6c030f756c5af67fe8d80f77f943654b4884
dengpeng <dengpeng>
2020-09-30 14:46:35 +0800

    vue项目第一次提交

---
+++ src/components/InnerPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InnerPage',
+  data () {
+    return {
+      msg: 'hello inner'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
