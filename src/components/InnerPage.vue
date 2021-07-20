65477c167e222622f2fbdada9af45afbe09151b8
dengpeng <dengpeng>
2021-07-20 10:16:37 +0800

    引入 vant组件库

--- src/components/InnerPage.vue
+++ src/components/InnerPage.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <button @click="jump()">点击一下</button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
+import { Button } from 'vant'
 export default {
+  components: {
+    [Button.name]: Button
+  },
   name: 'InnerPage',
   data () {
     return {
